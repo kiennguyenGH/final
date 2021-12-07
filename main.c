b773869089486241d0083bd34538dfa99bd40bac
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-07 09:01:43 +0000

    Add builtins

--- main.c
+++ main.c
@@ -4,6 +4,55 @@
 #include <stdio.h>
 #include <string.h>
 
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+char *builtin_str[] = {"cd", "help", "exit"};
+
+int (*builtin_func[]) (char**) = {&lsh_cd, &lsh_help, &lsh_exit};
+
+int lsh_num_builtins()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprint(stderr, "lsh: expected arguemnt to \"cd\"\n");
+    }
+    else
+    {
+        if (chdir(args[1]) != 0)
+        {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Kien Nguyen's LSH\n");
+    printf("Type program names and arugments, and hit enter.\n");
+    printf("The following are build in:\n");
+    for (i = 0; i < lsh_num_builtins(); i++)
+    {
+        printf(" %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args)
+{
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     lsh_loop();
